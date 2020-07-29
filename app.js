33ae0109dd0f7ffc3b12fb14647a6f6963031af4
dacardenasa <diego.cardenasaleg@gmail.com>
2020-07-28 21:36:53 -0500

    Merge module with app

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const app = express();
 const template = require('./template');
 
 app.get("/paragraphs", (req, res) => {
-  const templateString = template();
+  // const templateString = template();
+
+  let templateString = '';
+  let status = '';
+  for (let i = 1; i <= 50; i++){
+    status = (i % 2 === 0) ? 'Par' : 'Impar'; 
+    templateString += `<p>${i} Soy ${status}!</p>`;
+  }
+  // return templateString;
+
   res.send(templateString);
 });
 
